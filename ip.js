e109cdd6b1911f1c3be335279523370d1c48d1b3
Victory <victroryemmnuel@gmail.com>
2026-02-02 06:49:44 +0100

    js changes

--- ip.js
+++ ip.js
@@ -1,6 +1,27 @@
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+// import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 //https://geo.ipify.org/api/v2/country,city?apiKey=at_ug67FVmockmIE1t6TpVZKNat9d4EP&ipAddress=8.8.8.8
 import "leaflet/dist/leaflet.css"
 
 
- 
+ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
+function MapView() {
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      style={{ height: '400px', width: '100%' }}
+    >
+      <TileLayer
+        attribution='&copy; OpenStreetMap contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      <Marker position={[51.505, -0.09]}>
+        <Popup>Hello Lagos 🇳🇬</Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
+
+export default MapView;
