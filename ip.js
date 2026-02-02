32cc5844a557ef0425ab6e8e0c472d01ba9f535b
Victory <victroryemmnuel@gmail.com>
2026-02-02 06:31:38 +0100

    Moved from html to js

--- ip.js
+++ ip.js
@@ -1,3 +1,6 @@
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 //https://geo.ipify.org/api/v2/country,city?apiKey=at_ug67FVmockmIE1t6TpVZKNat9d4EP&ipAddress=8.8.8.8
 import "leaflet/dist/leaflet.css"
+
+
+ 
